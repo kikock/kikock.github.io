6559a36f75a9fd648acbd8fdb7bad0f3e1dc476b
kikock <34462958@qq.com>
2024-05-20 17:34:25 +0800

    博客代码优化

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/13/hello-world/","2024/05/17/Git命令汇总/"];function toRandomPost(){
+var posts=["2024/05/17/Git命令汇总/","2024/05/04/设计模式概述/","2024/05/20/设计模式_工厂模式/","2024/05/18/设计模式_单例模式/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
