1d78346a91265740fa380974009103ec14a41995
kikock <kikock@users.noreply.github.com>
2024-06-03 03:15:53 +0000

    deploy: kikock/hexo_home@c299a8190f7463457c883c8d268faaf6d1684002

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/22/CFWorkerTrojan/","2024/05/21/GitHubActions/","2024/05/17/Git命令汇总/","2024/05/21/docker安装教程/","2024/05/18/设计模式_单例模式/","2024/05/20/设计模式_工厂模式/","2024/05/04/设计模式概述/"];function toRandomPost(){
+var posts=["2024/05/22/CFWorkerTrojan/","2024/05/21/GitHubActions/","2024/05/17/Git命令汇总/","2024/06/03/Linux系统MCSM10面板搭建/","2024/05/21/docker安装教程/","2024/05/18/设计模式_单例模式/","2024/05/20/设计模式_工厂模式/","2024/05/04/设计模式概述/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
